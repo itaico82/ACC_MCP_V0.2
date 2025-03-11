63824c508fb562e2a1fa3b4ab339b07e8e587374
itaico82 <89577356+itaico82@users.noreply.github.com>
2025-03-11 22:49:53 +0200

    Create API package

---
+++ src/api/__init__.py
@@ -0,0 +1,9 @@
+"""
+API client for Autodesk Construction Cloud Issues API.
+
+This package provides a client for interacting with the Autodesk Construction Cloud Issues API.
+"""
+
+from src.api.client import ApiClient
+
+__all__ = ["ApiClient"]
